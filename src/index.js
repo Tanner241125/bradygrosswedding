bf918cd0e3a235695f6c1d1e85b3bcd57508488e
Tanner Gross <tanner241125@gmail.com>
2025-12-07 01:01:43 -0500

    fix whfwfhwfiowf

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ export default {
     if (url.pathname.startsWith("/api/")) {
       // Handle regular API request...
     }
-    return env.ASSETS.fetch(request);
+    return env.INDEX.fetch(request);
+  },
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith("/api/")) {
+      // Handle regular API request...
+    }
+    return env.ABOUT.fetch(request);
   },
 };
