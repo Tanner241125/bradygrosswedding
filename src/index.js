edb064b8d01f97d8cbd5aaf392f218d2806efac2
Tanner Gross <tanner241125@gmail.com>
2025-12-07 01:21:22 -0500

    fix i'm literally just trying anything at this point

--- src/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-    async fetch(request, env) {
-        const url = new URL(request.url);
-        if (url.pathname.startsWith("/api/")) {
-        // Handle regular API request...
-        }
-        return env.ASSETS.fetch(request);
-    },
-};
