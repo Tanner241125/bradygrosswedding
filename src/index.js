b945189bfcb468fbd9397bb5e746cad1b259f3cd
Tanner Gross <tanner241125@gmail.com>
2025-12-07 00:37:28 -0500

    fix weeeeeeeeee

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 export default {
-  async fetch(request, env, ctx) {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith("/api/")) {
+      // Handle regular API request...
+    }
     return env.ASSETS.fetch(request);
   },
 };
